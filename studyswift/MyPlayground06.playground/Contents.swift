ceacb44cdd0cda45f671050a46304783e5fd0e4c
SAEMI PARK <63420534+qkr7391@users.noreply.github.com>
2023-10-25 17:02:25 -0400

    Add files via upload
    operators

---
+++ studyswift/MyPlayground06.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+//operator
+
+//+a unary
+//a+b binary
+//a ? b : c ternary
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+var e = 5
+
+//a+
++a
+a + b
+a + b * c
+a + (b * c)
+(((a+b)*c)-d)*e
+a - b - c
+//Associative [left, right]
+
+let x = 32
+let y = 34
++x
+
+x+y
+
+-x
+-y
+
+x-y
+
+x*y
+x/y
+
+x%y
+let z = Double(x)
+let w = Double(y)
+z/w
+w/z
+
+
+//overflow
+//let num: Int8 = 9 * 9 * 9
+let num: Int = 9 * 9 * 9
