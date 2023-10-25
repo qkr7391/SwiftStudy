6f721bf01fb07256beccbd8c7f46288d080e7036
SAEMI PARK <63420534+qkr7391@users.noreply.github.com>
2023-10-25 16:49:46 -0400

    type

---
+++ studyswift/MyPlayground05.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+// Boolean
+var isReady : Bool = true
+var isDone = true
+
+
+// String
+
+"Have a good day"
+
+let str = "1"
+type(of: str) //string
+
+let ch: Character = "1"
+type(of: ch) //character
+
+//can not save "AA" --> over two is string
+let doubleCh: Character = "A"
+
+
+
+//Type Inference
+let num = 123
+type(of: num) //int
+
+let temp = 11.2
+type(of: temp) //double
+
+//Type Annotation
+//let name: Type = value
+//var name: Type = value
+let number: Int = 123
+let value: Double
+value = 11.3
+
