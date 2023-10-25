6e69ba32d6f7044e3953c29749726c57de7bc08d
SAEMI PARK <63420534+qkr7391@users.noreply.github.com>
2023-10-25 17:12:55 -0400

    update operator contents

--- studyswift/MyPlayground06.playground/Contents.swift
+++ studyswift/MyPlayground06.playground/Contents.swift
@@ -44,3 +44,74 @@ w/z
 //overflow
 //let num: Int8 = 9 * 9 * 9
 let num: Int = 9 * 9 * 9
+
+
+x == y
+"swift" == "Swift"
+
+x != y
+"semper" != "sempre"
+
+
+x > y
+x < y
+"swift" > "Swift"
+"swift" < "Swift"
+
+
+x >= y
+x <= y
+
+
+!true
+!false
+!(a < b)
+
+x > 100 && y > 10
+
+x > 100 || y > 10
+
+
+true && true //t
+true && false //f
+false && true //f
+false && false //f
+
+true || true //t
+true || false //t
+false || true //t
+false || false //f
+
+
+
+//condition ? expr1 : expr2
+
+let locco = 3
+let rocco = 30
+locco > rocco ? print(locco) : print(rocco)
+
+if locco > rocco {
+    print(locco)
+} else {
+    print(rocco)
+}
+
+//assignment operator
+let temp = 12
+//12 + 3 = 12 //>> error
+var ttemp = 12
+ttemp = 34
+if ttemp == 56 {
+    print (ttemp)
+}
+
+
+//Addition Assignment Operator
+a += b
+a = a + b
+
+var q = 0
+q = q + 1
+q += 1
+
+
