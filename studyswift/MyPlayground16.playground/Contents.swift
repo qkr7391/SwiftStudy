a5f040d0e789fddb9d251881fc62c36a4659d38b
SAEMI PARK <63420534+qkr7391@users.noreply.github.com>
2023-10-31 21:31:29 -0400

    funciton, function defining

---
+++ studyswift/MyPlayground16.playground/Contents.swift
@@ -0,0 +1,22 @@
+//Functions
+
+
+//01. calling functions
+//functionName(parameters)
+
+print("Hello")
+print()
+
+
+//02. Defining Functions
+/*
+ func name(parameters) -> ReturnType { // << function head
+    statements // << funcion body
+ }
+*/
+
+func sayHello(){
+    print("Hello, Swift")
+}
+
+sayHello() //call-site / caller
