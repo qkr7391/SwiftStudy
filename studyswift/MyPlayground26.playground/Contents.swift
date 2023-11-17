3b4d244ea0bf9eb54a2dc09b2b9f4b590c30be58
SAEMI PARK <63420534+qkr7391@users.noreply.github.com>
2023-11-16 23:53:38 -0500

    not completed

---
+++ studyswift/MyPlayground26.playground/Contents.swift
@@ -0,0 +1,18 @@
+
+// Collection Types
+// Foundation Collectio:(class) and Swift Collection:(objective)
+
+// Foundation Collection --> NSArray, NSDictionry, NSSet
+//                           Object
+//                           no limit for type elements(String Obejct, Number Object ...)
+
+// Swift Collection  --> Array, Dictionary, Set
+//                       Object, value
+//                       One type element can be saved (String only / Number only ...)
+
+// Variable of Collection
+// Copy-on-Write
+
+
+
+
